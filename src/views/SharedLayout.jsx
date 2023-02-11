68747cafc0d618e8acbf3e0e32e3727eeb12a83e
calvickauer <calvickauer@gmail.com>
2023-02-10 18:45:05 -0800

    nav bar

--- src/views/SharedLayout.jsx
+++ src/views/SharedLayout.jsx
@@ -6,11 +6,13 @@ const SharedLayout = () => {
     return (
         <>
             <header>
-                <div className="nav-area">
-                    <NavLink to="/test/public" className='logo'>public_test_route</NavLink>
-                    <NavLink to="/test/protected" className='logo'>protected_test_route</NavLink>
-                    <NavLink to="/test/admin" className='logo'>admin_test_route</NavLink>
-                    <NavBarButtons />
+                <div className="nav__area">
+                    <h1 className="nav__title">Thriver!</h1>
+                    <NavLink to="/" className='nav__link'>Home</NavLink>
+                    <NavLink to="/test/public" className='nav__link'>public_test_route</NavLink>
+                    <NavLink to="/test/protected" className='nav__link'>protected_test_route</NavLink>
+                    <NavLink to="/test/admin" className='nav__link'>admin_test_route</NavLink>
+                    {/* <NavBarButtons /> */}
                 </div>
             </header>
             <section>

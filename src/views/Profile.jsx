67f2e87b2ea10bf042ce9555e1f0eba1260cc48a
calvickauer <calvickauer@gmail.com>
2023-02-13 21:22:01 -0800

    Profile page layout

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,20 +1,35 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import { CodeSnippet } from "../components/code-snippet";
 
 const Profile = () => {
+  const { user } = useAuth0();
+  console.log(user)
 
-    const { user } = useAuth0();
-    console.log(user);
+  if (!user) {
+    return null;
+  }
 
-    if (!user) {
-        return null;
-    }
-
-    return (
-        <div className="Profile__container">
-            <h1>Profile page</h1>
+  return (
+      <div className="profile-page__container">
+        <div className="profile__titleContainer">
+        <h2 className="profile__title">Logged into {user.name}'s Profile </h2>
         </div>
-    )
-}
+            <div className="profile__avatarContainer">
+              <img
+                src={user.picture}
+                alt="Profile"
+                className="profile__avatar"
+              />
+            </div>
+            <div className="profile__details">
+              <CodeSnippet
+                title="Decoded ID Token"
+                code={JSON.stringify(user, null, 2)}
+              />
+            </div>
+      </div>
+  );
+};
 
 export default Profile;

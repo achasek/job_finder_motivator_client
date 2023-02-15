3579b1ae97d5c8dee99609808d015c1be8d960b0
TannerEsch <eschmanntanner@gmail.com>
2023-02-15 16:18:30 -0500

    added overflow hidden to the calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,11 +6,12 @@ export default function CalendarGfg() {
   const [value, onChange] = useState(new Date());
   
   return (
-    <div class="calendarWrapper"style={{position:"absolute", left:"50%", display:"flex", justifyContent:"center", alignItems:"center", top:"55%", backgroundColor:"#F2F2F2", height:"40%", width:"20vw",
+    <div class="calendarWrapper"style={{position:"absolute", left:"50%", overflow:"hidden", display:"flex", justifyContent:"center", alignItems:"center", top:"55%", backgroundColor:"#F2F2F2", height:"40%", width:"20vw",
     borderRadius:"50px", boxShadow:"0 0 15px", transform: "translateX(140%)"}}>
     <div style={{position:"absolute", backgroundColor:"white", height:"fit-content", width:"20vw"}}>
         <h3 style={{position:"absolute", left:"38%", top:"-19%"}}>Calendar</h3>
       <Calendar
+      
         onChange={onChange}
         value={value}
       />

86af43e05c1f61470f748fee9c82320a03f7a63b
calvickauer <calvickauer@gmail.com>
2023-03-03 08:58:04 -0800

    hide profile tab if not logged in

--- src/views/SharedLayout.jsx
+++ src/views/SharedLayout.jsx
@@ -1,7 +1,11 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { NavBarButtons } from "../components";
+import { ConstContext, UserContext } from '../App';
+import React, { useContext } from "react";
 
-const SharedLayout = () => {            
+const SharedLayout = () => {          
+    const { currUser } = useContext(UserContext);  
+    console.log("current user",currUser)
     
 
     return (
@@ -9,13 +13,12 @@ const SharedLayout = () => {
             <header className="nav__header">
                 <div className="nav__area">
                     <a href="/">
-                    <div className="nav__logo"></div>
+                        <div className="nav__logo"></div>
                     </a>
                     <div className="nav__block"></div>
                     <NavLink to="/" className='nav__link'>Home</NavLink>
-                    {/* <NavLink to="/about" className='nav__link'>About</NavLink> */}
                     <NavLink to="/dashboard" className='nav__link'>My Dash</NavLink>
-                    <NavLink to="/user/profile" className='nav__link'>My Profile</NavLink>
+                    {currUser?._id && <NavLink to="/user/profile" className='nav__link'>My Profile</NavLink>}
                     {/* <NavLink to="/test/public" className='nav__link'>public-test</NavLink>
                     <NavLink to="/test/protected" className='nav__link'>protected-test</NavLink>
                     <NavLink to="/test/admin" className='nav__link'>admin-test</NavLink> */}
@@ -28,6 +31,7 @@ const SharedLayout = () => {
             </section>
         </>
     );
+    
 }
 
 
